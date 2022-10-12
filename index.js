2074d6f1a8c52ae27884c3f69c06a8faa7496a87
NashJ99 <jnash4@una.edu>
2022-10-12 18:27:25 -0500

    cleanup

--- index.js
+++
@@ -1,9 +0,0 @@
-$(".delete").click(function(){
-    console.log("prepare for termination");
-    var id = $(this).attr("id");
-    fetch(`name/name_id:${id}`, {
-        method:"delete",
-        headers: {'Content-Type': 'applications/json'}
-      })
-    
-    })
